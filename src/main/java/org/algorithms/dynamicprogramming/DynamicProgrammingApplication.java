bbee2542848c0b2ce4681a14dcf45f182068b30d
onlucscrin <vinutha.m.reddy@gmail.com>
2021-08-31 19:34:43 -0400

    Initial Commit Base Version

---
+++ src/main/java/org/algorithms/dynamicprogramming/DynamicProgrammingApplication.java
@@ -0,0 +1,13 @@
+package org.algorithms.dynamicprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/algorithms/dynamicprogramming/DynamicProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.algorithms.dynamicprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
